02b4d1faf9740174cc12a7ad3f09bd900299f306
edkazcarlson <ekcarlson@wpi.edu>
2020-03-15 15:37:10 -0400

    dummy change to check git login

--- SQLType.h
+++ SQLType.h
@@ -1,11 +1,33 @@
 #ifndef SQLTYPE_H
 #define SQLTYPE_H
 
+#include <QtGlobal>
 
-class SQLType
-{
+class SQLType{
 public:
     SQLType();
+    QString toString();
 };
 
+class sizedSQLType : public SQLType{
+public:
+    sizedSQLType();
+    short sizeAttributes;
+    QString toString();
+private:
+};
+
+
 #endif // SQLTYPE_H
+/*TODO
+ *Make set of Qstrings that contain all the SQL types for PLSQL (add others later)
+ * Make a decorator for single sized, make another for double sized
+ * when the user changes the type make it so that it checks the new type against lists of
+ * single or double sized ones then decide whether to add the decorator
+ */
+
+ /* Default no size has all the types and a draw/toString method that can be used in order to put it on the
+  * TableAttribute that everyone can call
+  *
+  * For FKS,make it end-user responsiblity for now
+  */
